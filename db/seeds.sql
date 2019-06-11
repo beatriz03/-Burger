d8ae155fdf855c7f3febfaeca9e454c973f09834
Beatriz <renderosb@gmail.com>
2019-06-11 12:53:39 -0400

    files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', false);
